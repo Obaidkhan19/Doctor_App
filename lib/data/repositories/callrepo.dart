22bf85b1eb2d13a1ae02926877b3bfe57142d1b0
darklips97351 <dark.lips1@outlook.com>
2023-10-30 11:13:53 +0500

    commit

--- lib/data/repositories/callrepo.dart
+++ lib/data/repositories/callrepo.dart
@@ -7,37 +7,37 @@ import 'package:doctormobileapplication/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-   callOpenPrescription(context,consultingqueuewaitholdresponse data) async {
-    var body = {
-    "PatientId":data.patientId,
+callOpenPrescription(context, consultingqueuewaitholdresponse data) async {
+  String dt = await LocalDb().getDeviceToken();
+  var body = {
+    "PatientId": data.patientId,
     "DoctorId": data.doctorId,
-    "VisitNo":data.visitNo,
-    "BranchId":data.branchId,
-    "DeviceToken":"dyeh4X8GThWroZ4c8qK_em:APA91bHo1gXBNvMwloR0vxjpMLlZ5Lauzw-v6_Zqba9Eytkzs5AYXDjxJ5m_n3ZoJyHUFEdGyl0fHgDYmOXvEmPKV2dSOPzGlvEO9twbDeVZflhb8ccC5EMJ0dsePLF8xehpNDsAoBFT",
-    "PrescribedInValue":"2".toString(),
-    "IsFirstTimeVisit":data.isFirstTimeVisit==1?"true":"false",
-    "IsOnline":"true"
-    };
+    "VisitNo": data.visitNo,
+    "BranchId": data.branchId,
+    "DeviceToken": dt,
+    "PrescribedInValue": "2".toString(),
+    "IsFirstTimeVisit": data.isFirstTimeVisit == 1 ? "true" : "false",
+    "IsOnline": "true"
+  };
 
-    // var headers = {'Content-Type': 'application/json'};
-    try {
-      var response = await http.post(
-        Uri.parse(AppConstants.opencallprescription),
-        body: body,
-      );
-      log(jsonEncode(body));
-      if (response.statusCode == 200) {
-        var result = jsonDecode(response.body);
-        if (result['Status'] == 1) {
-          log("Success");
-          return result['Status'];
-        } else {
-          return result['Status'];
-        }
+  // var headers = {'Content-Type': 'application/json'};
+  try {
+    var response = await http.post(
+      Uri.parse(AppConstants.opencallprescription),
+      body: body,
+    );
+    log(jsonEncode(body));
+    if (response.statusCode == 200) {
+      var result = jsonDecode(response.body);
+      if (result['Status'] == 1) {
+        log("Success");
+        return result['Status'];
+      } else {
+        return result['Status'];
       }
-    } catch (e) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(const SnackBar(content: Text("Something went wrong!")));
     }
+  } catch (e) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(const SnackBar(content: Text("Something went wrong!")));
   }
 }
